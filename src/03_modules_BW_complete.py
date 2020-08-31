b94c33f70c773614687fde18769fd986a4ce7f10
Ava Wingfield <bwitter770@gmail.com>
2020-08-31 18:45:05 -0400

    completed most files

--- src/03_modules_BW_complete.py
+++ src/03_modules_BW_complete.py
@@ -7,25 +7,27 @@ level operating system functionality.
 
 import sys
 # See docs for the sys module: https://docs.python.org/3.7/library/sys.html
-
+help(sys)
+#%%
 # Print out the command line arguments in sys.argv, one per line:
-# YOUR CODE HERE
+print([sys.argv[val] for val in range(len(sys.argv))])
 
 # Print out the OS platform you're using:
-# YOUR CODE HERE
+print(sys.platform )
 
 # Print out the version of Python you're using:
-# YOUR CODE HERE
+print(sys.version)
 
-
+#%%
 import os
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
-
+help(os)
+#%%
 # Print the current process ID
-# YOUR CODE HERE
-
+print(os.getpid())
+#%%
 # Print the current working directory (cwd):
-# YOUR CODE HERE
-
+print(os.getcwd())
+#%%
 # Print out your machine's login name
-# YOUR CODE HERE
+print(os.getlogin())
